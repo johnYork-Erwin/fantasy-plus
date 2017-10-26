6bdf62e4335d9210a08a600213496ae3e377baa3
John York-Erwin <solidseussian@gmail.com>
2017-10-26 13:57:09 -0700

    modularized scrubbing and moved it to server side

--- server/routes/external.js
+++ server/routes/external.js
@@ -1,29 +1,47 @@
 'use strict';
-let key = '3YGwL0H9Chk71QfcUMKzPjiTx2qBuopl'
+let key = '3YGwL0H9Chk71QfcUMKzPjiTx2qBuopl';
 
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
+const scrubHelper = require('../../src/scrubHelper.js')
 
-router.get('/external/game/:id', (req, res, next) => {
-  axios.post(`https://profootballapi.com/game?api_key=${key}&game_id=${req.params.id}`).then(result => {
-    res.send(result.data)
-  })
-  .catch(err => {
-    console.log(err)
-  })
-})
-
-router.get('/external/schedule/:week', (req, res, next) => {
-  axios.post(`https://profootballapi.com/schedule?api_key=${key}&year=2017&week=${req.params.week}`).then(result =>{
-    let idArray = result.data.map(el => {
-      return el.id;
+function getGame(gameId) {
+  return new Promise(function (resolve, reject) {
+    axios.post(`https://profootballapi.com/game?api_key=${key}&game_id=${gameId}`).then(result => {
+      resolve(result.data);
     })
-    res.send(idArray)
+    .catch(err => {
+      reject(console.log('error calling for game by id on line 15'))
+    })
   })
-  .catch(err => {
-    console.log(err)
+}
+
+function getSchedule(weekNumber) {
+  return new Promise(function (resolve, reject) {
+    axios.post(`https://profootballapi.com/schedule?api_key=${key}&year=2017&week=${weekNumber}`).then(result =>{
+      let idArray = result.data.map(el => {
+        return el.id;
+      })
+      resolve(idArray);
+    })
+    .catch(err => {
+      reject(console.log('error calling for schedule'))
+    })
   })
+}
+
+
+router.get('/external/update', (req, res, next) => {
+  getSchedule(7).then(result => {
+    getGame(result[5].toString()).then(game => {
+      let thingy = scrubHelper(game)
+      let players = thingy.players;
+      let teams = thingy.teams;
+    })
+    .catch(err => console.log('error getting game by id'))
+  })
+  .catch(err => console.log('error fetching schedule'))
 })
 
 module.exports = router;
---
+++ src/scrubHelper.js
@@ -0,0 +1,239 @@
+const deepcopy = require('deepcopy');
+
+const playerStats = {
+  team: '',
+  rushAttempts: 0,
+  rushYards: 0,
+  rushTd: 0,
+  rec: 0,
+  recTargets: 0,
+  recYards: 0,
+  recTd: 0,
+  returnTd: 0,
+  twoPoints: 0,
+  fumbles: 0,
+  passAttempts: 0,
+  passCompletions: 0,
+  passYards: 0,
+  passTd: 0,
+  int: 0,
+  plays: []
+}
+
+const teamStats = {
+  teamCode: '',
+  score: 0,
+  oppScore: 0,
+  result: '',
+  opp: '',
+  home: false,
+  week: 0,
+  rush: {
+    attempts: 0,
+    yards: 0,
+    touchdowns: 0,
+  },
+  pass: {
+    attempts: 0,
+    completions: 0,
+    yards: 0,
+    touchdowns: 0,
+  },
+  gameScript: [],
+}
+
+const scrubForTeams = function(obj) {
+  const homeTeam = deepcopy(teamStats);
+  homeTeam.home = true;
+  const awayTeam = deepcopy(teamStats);
+  homeTeam.teamCode = obj.home.team;
+  homeTeam.opp = obj.home.opponent;
+  awayTeam.teamCode = obj.home.opponent;
+  awayTeam.opp = obj.home.team;
+  homeTeam.week = obj.week;
+  awayTeam.week = obj.week;
+  homeTeam.score = obj.home_score;
+  homeTeam.oppScore = obj.away_score;
+  awayTeam.score = obj.away_score;
+  awayTeam.oppScore = obj.home_score;
+  if (homeTeam.score > awayTeam.score) {
+    homeTeam.result = 'W';
+    awayTeam.result = 'L';
+  } else if (homeTeam.score === awayTeam.score) {
+    homeTeam.result = 'D';
+    awayTeam.result = 'D';
+  } else {
+    homeTeam.result = 'L';
+    awayTeam.result = 'W';
+  }
+  for (let passer in obj.home.stats.passing) {
+    passer = obj.home.stats.passing[passer];
+    homeTeam.pass.attempts += passer.attempts;
+    homeTeam.pass.completions += passer.completions;
+    homeTeam.pass.yards += passer.yards;
+    homeTeam.pass.touchdowns += passer.touchdowns;
+  }
+  for (let passer in obj.away.stats.passing) {
+    passer = obj.away.stats.passing[passer];
+    awayTeam.pass.attempts += passer.attempts;
+    awayTeam.pass.completions += passer.completions;
+    awayTeam.pass.yards += passer.yards;
+    awayTeam.pass.touchdowns += passer.touchdowns;
+  }
+  for (let runner in obj.home.stats.rushing) {
+    runner = obj.home.stats.rushing[runner];
+    homeTeam.rush.attempts += runner.attempts;
+    homeTeam.rush.yards += runner.yards;
+    homeTeam.rush.touchdowns += runner.touchdowns;
+  }
+  for (let runner in obj.away.stats.rushing) {
+    runner = obj.away.stats.rushing[runner];
+    awayTeam.rush.attempts += runner.attempts;
+    awayTeam.rush.yards += runner.yards;
+    awayTeam.rush.touchdowns += runner.touchdowns;
+  }
+  let teams = {};
+  teams[homeTeam.teamCode] = homeTeam;
+  teams[awayTeam.teamCode] = awayTeam;
+  return teams;
+}
+
+const scrubForPlayers = function(obj) {
+  let players = {};
+  //merges things so we can have an easier time looping through them
+  const rushing = Object.assign(obj.away.stats.rushing, obj.home.stats.rushing)
+  const passing = Object.assign(obj.away.stats.passing, obj.home.stats.passing)
+  const receiving = Object.assign(obj.away.stats.receiving, obj.home.stats.receiving)
+  let fumbles = {};
+  //merges fumbles, there may be no stats for fumbles
+  if (obj.away.stats.fumbles && obj.home.stats.fumbles) {
+    fumbles = Object.assign(obj.away.stats.fumbles, obj.home.stats.fumbles)
+  } else if (obj.away.stats.fumbles) {
+    fumbles = obj.away.stats.fumbles;
+  } else if (obj.home.stats.fumbles) {
+    fumbles = obj.home.stats.fumbles;
+  }
+  //loop through all the stats in the game for running plays
+  for (let run in rushing) {
+    run = rushing[run]
+    let current = run.name;
+    let object = deepcopy(playerStats);
+    object.rushTargets = run.attempts;
+    object.rushYards = run.yards;
+    object.rushTd = run.touchdowns;
+    object.twoPoints = run.two_point_makes;
+    players[current] = object;
+  }
+  //loop through all the stats in the game for receiving
+  for (let reception in receiving) {
+    reception = receiving[reception];
+    let object;
+    if (players.hasOwnProperty(reception.name)) {
+      object = players[reception.name];
+    } else {
+      object = deepcopy(playerStats);
+    }
+    object.recYards = reception.yards;
+    object.recTd = reception.touchdowns;
+    object.rec = reception.receptions;
+    object.twoYards = reception.two_point_makes;
+    players[reception.name] = object;
+  }
+  //loop through all the stats in the game for passing
+  for (let pass in passing) {
+    pass = passing[pass]
+    let object;
+    if (players.hasOwnProperty(pass.name)) {
+      object = players[pass.name]
+    } else {
+      object = deepcopy(playerStats);
+    }
+    object.passAttempts = pass.attempts;
+    object.passCompletions = pass.completions;
+    object.int = pass.interceptions;
+    object.passTd = pass.touchdowns;
+    object.twoPoints = pass.two_point_makes;
+    object.passYards = pass.yards;
+    players[pass.name] = object;
+  }
+  //track fumbles
+  for (let fumble in fumbles) {
+    if (fumbles[fumble].fumbles_lost && !players.hasOwnProperty(fumbles[fumble].name)) {
+      players[fumbles[fumble].name].fumbles ++;
+    }
+  }
+  return players;
+}
+
+const scrubGame = function(obj) {
+  const drives = Object.assign(obj.away.drives, obj.home.drives)
+  let teams = scrubForTeams(obj);
+  let players = scrubForPlayers(obj);
+  //loops through all the drives of the game
+  let timeLeft = 3600;
+  let gameScript = []
+  for (let drive in drives) {
+    drive = drives[drive];
+    //for tracking gameScript
+    timeLeft -= drive.postime;
+    let score = 0;
+    if (drive.result !== 'Punt' || 'Missed FG') {
+      if (drive.result === 'Touchdown') {
+        score += 6;
+        if (drive.plays[drive.plays.length-1].description.includes('extra point is GOOD')) {
+          score++;
+        }
+      } else if (drive.result === 'Field Goal') {
+        score += 3;
+      }
+      let builder = [];
+      let homeScore = 0;
+      let awayScore = 0;
+      builder.push(timeLeft);
+      if (drive.posteam === obj.home.team) {
+        homeScore += score;
+      } else {
+        awayScore += score;
+      }
+      if (gameScript.length > 0) {
+        awayScore += gameScript[gameScript.length-1][2]
+        homeScore += gameScript[gameScript.length-1][1]
+      }
+      builder.push(homeScore);
+      builder.push(awayScore);
+      gameScript.push(builder);
+      //ALSO NEED TRAKCING INTERCEPTIONS / FUMBLES FOR TD!
+      //NEED SAFETYES!
+    }
+    //for tracking plays for players
+    for (let play in drive.plays) {
+      play = drive.plays[play].description
+      //Here we have the description of the play
+      if (!play.includes('- No Play')) {
+        if (!play.match(/(kicks|Holder-|punt)/g)) {
+          let offensivePlayers = play.match(/\w\.\w+(\s|\.)/g);
+          if (offensivePlayers) {
+            offensivePlayers = offensivePlayers.map(el => {
+              return el.slice(0, el.length-1);
+            })
+          }
+          if (offensivePlayers) {
+            for (let i = 0; i < offensivePlayers.length; i++) {
+              if (players.hasOwnProperty(offensivePlayers[i])) {
+                players[offensivePlayers[i]].plays.push(play);
+                if (i === 1) {
+                  players[offensivePlayers[i]].recTargets++
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  teams[obj.home.team].gameScript = gameScript;
+  teams[obj.away.team].gameScript = gameScript;
+  return {players: players, teams: teams}
+}
+
+module.exports = scrubGame;
